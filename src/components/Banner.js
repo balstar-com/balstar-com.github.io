98a477f395ce0763c46d828f674e57e75d2135c0
David Edwards <david@more.fool.me.uk>
2022-01-13 18:31:16 +0000

    Add Tweak wording and footer formatting

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => (
                 <h1>Hi! We are Balstar,<br/> a software engineering consultancy</h1>
             </header>
             <div className="content">
-                <p>Our software engineering experience enables <br/> successful project delivery</p>
+                <p>Our software engineering experience <br/> leads to successful project outcomes</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Learn more</a></li>
                 </ul>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,11 +10,7 @@ const Footer = (props) => (
                 <li><a href="https://github.com/purple52" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                 <li><a href="https://www.linkedin.com/company/balstar" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
-            <ul className="copyright">
-                <li>&copy; Balstar Limited. All rights reserved</li>
-                <li className="website">Website template credit: <a href="https://html5up.net">HTML5 UP</a></li>
-            </ul>
-            <p className="company">Balstar Limited is registered as company no. 8550201 in England and Wales. Registered address: 10 Penland Road, Haywards Heath, West Sussex, RH16 1PW</p>
+            <p className="company">&copy; Balstar Limited. All rights reserved. Template by: <a href="https://html5up.net">HTML5 UP</a>. Balstar Limited is registered as company no. 8550201 in England and Wales. Registered address: 10 Penland Road, Haywards Heath, West Sussex, RH16 1PW</p>
         </div>
     </footer>
 )
